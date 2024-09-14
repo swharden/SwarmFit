005430cd583ec51ce3605db97ee691cdec9e49be
Scott W Harden <swharden@gmail.com>
2024-09-14 14:38:50 -0400

    FitSolution: ToString() overload

--- src/SwarmFit/FitSolution.cs
+++ src/SwarmFit/FitSolution.cs
@@ -7,4 +7,10 @@ public class FitSolution(double[] vars, double[] errors, TimeSpan elapsed, int i
     public TimeSpan Elapsed { get; } = elapsed;
     public int Iterations { get; } = iterations;
     public int Particles { get; } = particles;
+
+    public override string ToString()
+    {
+        string vars = string.Join(", ", Variables.Select(x => x.ToString()));
+        return $"Solution [{vars}] achieved in {Elapsed.TotalMilliseconds} msec using {Particles} particles and {Iterations} iterations";
+    }
 };
