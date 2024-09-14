283531980877f261636316342ecf811574d7225a
Scott W Harden <swharden@gmail.com>
2024-09-13 21:16:19 -0400

    QuickFit.Solve()

---
+++ src/SwarmFit/QuickFit.cs
@@ -0,0 +1,24 @@
+﻿namespace SwarmFit;
+
+public static class QuickFit
+{
+    public static double[] Solve(double[] xs, double[] ys, Func<double, double[], double> func, double[] minVars, double[] maxVars, int particles = 100, int iterations = 10_000)
+    {
+        if (minVars.Length != maxVars.Length)
+        {
+            throw new ArgumentException($"{nameof(minVars)} and {nameof(maxVars)} must have equal length");
+        }
+
+        VariableLimits[] limits = Enumerable
+            .Range(0, minVars.Length)
+            .Select(x => new VariableLimits(minVars[x], maxVars[x]))
+            .ToArray();
+
+        SwarmFitter fit = new(xs, ys, func, limits)
+        {
+            NumParticles = 100
+        };
+
+        return fit.Solve(iterations).Variables;
+    }
+}
